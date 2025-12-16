8e95aa43210954fdb9011e35ded7d9e2837a8444
jianzhnie <jianzhnie@126.com>
2025-12-16 17:28:41 +0800

    Update scaletorch

--- scaletorch/utils/__init__.py
+++ scaletorch/utils/__init__.py
@@ -1,3 +1,5 @@
+from scaletorch.utils.device import (get_current_device, get_device,
+                                     get_device_count, get_dist_info)
 from scaletorch.utils.env_utils import (cleanup_dist, get_system_info,
                                         init_dist_pytorch)
 from scaletorch.utils.lenet_model import LeNet
@@ -6,5 +8,6 @@ from scaletorch.utils.path import check_file_exist, mkdir_or_exist
 
 __all__ = [
     'get_system_info', 'LeNet', 'init_dist_pytorch', 'cleanup_dist',
-    'get_logger', 'get_outdir', 'check_file_exist', 'mkdir_or_exist'
+    'get_logger', 'get_outdir', 'check_file_exist', 'mkdir_or_exist',
+    'get_device', 'get_dist_info', 'get_current_device', 'get_device_count'
 ]
